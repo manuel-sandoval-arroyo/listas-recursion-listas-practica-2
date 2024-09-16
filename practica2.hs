48eab386f3adf1cddc2d12e481152060f9e83dca
Manuel Sandoval <manusandoval05@gmail.com>
2024-09-16 11:41:57 -0600

    initial practice

--- practica2.hs
+++ practica2.hs
@@ -1,27 +1,42 @@
 --------------- Listas y recursión ---------------
 
 longitud :: [a] -> Int
-longitud _ = undefined
+longitud [] = 0
+longitud (_ : xs) = 1 + longitud xs
 
 sumaLista :: Num a => [a] -> a
-sumaLista _ = undefined
+sumaLista [] = 0
+sumaLista (x : xs) = x + sumaLista xs
 
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento _ = undefined
+agregaElemento [] a cabeza = [a]
+agregaElemento xs a cabeza = 
+    if cabeza then a : xs 
+    else xs ++ [a]
 
 maximoLista :: (Num a, Ord a) => [a] -> a
-maximoLista _ = undefined
+maximoLista [] = 0
+maximoLista [x] = x
+maximoLista (x : xs) = max x (maximoLista xs)
 
 indice :: [a] -> Int -> a
-indice _ = undefined
+indice [] _ = error "Indice fuera de rango"
+indice (x : xs) 0 = x
+indice (x : xs) n =
+    if n >= 0 && n < longitud xs then indice xs (n-1)
+    else error "Indice fuera de rango"
+
 
 --------------- Listas por comprehensión ---------------
 
 divisores :: Int -> [Int]
-divisores _ = undefined
+divisores 0 = []
+divisores n = [x | x <- [1..n], n `mod` x == 0]
 
 conjunto :: Eq a => [a] -> [a]
-conjunto _ = undefined
+conjunto [] = []
+conjunto (x : xs) = x : conjunto [y | y <- xs, y /= x]
 
 numerosPares :: [Int] -> [Int]
-numerosPares _ = undefined
+numerosPares [] = []
+numerosPares xs = [x | x <- xs, x `mod` 2 == 0]
